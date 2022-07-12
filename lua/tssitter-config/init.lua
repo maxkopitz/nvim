90e33f034ce86f9e87fc63c7b68d5d8792bd5e45
Max Kopitz <mlkopitz@umich.edu>
2022-07-12 10:16:22 -0400

    Added ts to init.lua so it would actually run on open

--- lua/tssitter-config/init.lua
+++ lua/tssitter-config/init.lua
@@ -1,10 +1,12 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "javascript" },
+  ensure_installed = { "dockerfile", "c", "lua", "rust", "javascript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
