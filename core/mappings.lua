bc8b9a478f5dc56e3fa974c38fd243722c74b70d
Max Kopitz <mlkopitz@umich.edu>
2022-09-21 14:57:03 -0400

    Adding mapping

---
+++ core/mappings.lua
@@ -0,0 +1,57 @@
+local keymap = vim.keymap
+local api = vim.api
+
+
+--> nvim tree keymap.setpings <--
+keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>')
+keymap.set('n', '<leader>f', ':NvimTreeFocus<CR>')
+
+--> BufferLine keymap.setpings <--
+keymap.set("n", "<Tab>,", ":BufferLinePrevious<CR>")
+keymap.set("n", "<Tab>.", ":BufferLineNext<CR>")
+keymap.set("n", "<Tab><", ":BufferLineMovePrevious<CR>")
+keymap.set("n", "<Tab>>", ":BufferLineMoveNext<CR>")
+keymap.set("n", "<Tab>1", ":BufferLineGoToBuffer 1<CR>")
+keymap.set("n", "<Tab>2", ":BufferLineGoToBuffer 2<CR>")
+keymap.set("n", "<Tab>3", ":BufferLineGoToBuffer 3<CR>")
+keymap.set("n", "<Tab>4", ":BufferLineGoToBuffer 4<CR>")
+keymap.set("n", "<Tab>5", ":BufferLineGoToBuffer 5<CR>")
+keymap.set("n", "<Tab>6", ":BufferLineGoToBuffer 6<CR>")
+keymap.set("n", "<Tab>7", ":BufferLineGoToBufer 7<CR>")
+keymap.set("n", "<Tab>8", ":BufferLineGoToBuffer 8<CR>")
+keymap.set("n", "<Tab>9", ":BufferLineGoToBuffer 9<CR>")
+keymap.set("n", "<Tab>0", ":BufferLineLast<CR>")
+keymap.set("n", "<Tab>c", ":BufferLineClose<CR>")
+keymap.set("n", "<C-p>", ":BufferLinePick<CR>")
+keymap.set("n", "<leader>bb", ":BufferLineOrderByBufferNumber<CR>")
+keymap.set("n", "<leader>bd", ":BufferLineOrderByDirectory<CR>")
+keymap.set("n", "<leader>bl", ":BufferLineOrderByLanguage<CR>")
+
+--> Vista keymap.setpings <--
+keymap.set("n", "<leader>v", ":Vista coc<CR>")
+
+--> Git fugitive <--
+keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git status" })
+keymap.set("n", "<leader>gw", "<cmd>Gwrite<cr>", { desc = "Git add" })
+keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "Git commit" })
+keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "Git diff" })
+keymap.set("n", "<leader>gpl", "<cmd>Git pull<cr>", { desc = "Git pull" })
+keymap.set("n", "<leader>gpu", "<cmd>15 split|term git push<cr>", { desc = "Git push" })
+
+-- Edit and reload nvim config file quickly
+keymap.set("n", "<leader>ev", "<cmd>tabnew $MYVIMRC <bar> tcd %:h<cr>", {
+  silent = true,
+  desc = "open init.lua",
+})
+
+keymap.set("n", "<leader>sv", "", {
+  silent = true,
+  desc = "reload init.lua",
+  callback = function()
+    vim.cmd([[
+      update $MYVIMRC
+      source $MYVIMRC
+    ]])
+    vim.notify("Nvim config successfully reloaded!", vim.log.levels.INFO, { title = "nvim-config" })
+  end,
+})
