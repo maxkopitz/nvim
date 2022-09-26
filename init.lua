938ea921e3df93e0c770f1c7ff5bff1b0e502807
Max Kopitz <mlkopitz@umich.edu>
2022-09-25 23:22:10 -0400

    Formatting

--- init.lua
+++ init.lua
@@ -27,5 +27,4 @@ for _, name in ipairs(core_conf_files) do
   local path = string.format("%s/core/%s", vim.fn.stdpath("config"), name)
   local source_cmd = "source " .. path
   vim.cmd(source_cmd)
-
 end
