27ade4155cd0fbb706c5fc0e2655ef170aa91ae7
Max Kopitz <mlkopitz@umich.edu>
2022-10-25 15:36:42 -0400

    Added keymapping to exit termnal
    Keymapping to exit toggleterm

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,6 +1,19 @@
-require('toggleterm').setup {
+-- TODO, dont take up whole bottom leave room for filetree
+local toggleterm = require('toggleterm')
+
+toggleterm.setup {
   size = 13,
   open_mapping = [[<c-\>]],
   direction = 'horizontal',
-  start_in_insert = true,
+  hide_numbers = true, -- Hide the number colun in toggleterm buffers
 }
+
+-- Terminal window mappings
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
