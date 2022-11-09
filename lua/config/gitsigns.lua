f374ebd4c7ded84e20ea0564abe86d5f31eec2ba
Max Kopitz <mlkopitz@umich.edu>
2022-11-09 16:53:04 -0500

    config(gitsigns): config

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -8,7 +8,7 @@ gs.setup {
     topdelete = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
     changedelete = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
   },
-  word_diff = false,
+  word_diff = true,
   on_attach = function(bufnr)
     local function map(mode, l, r, opts)
       opts = opts or {}
@@ -49,9 +49,9 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = '*',
   callback = function()
     vim.cmd [[
-      hi GitSignsChangeInline guibg=fg guifg=bg
-      hi GitSignsAddInline guibg=fg guifg=bg
-      hi GitSignsDeleteInline guibg=fg guifg=bg
+      hi GitSignsChangeInline gui=reverse
+      hi GitSignsAddInline gui=reverse
+      hi GitSignsDeleteInline gui=reverse
     ]]
   end,
 })
