a3d73083364180f2e20d649bfca7e88cc23b3a2e
Max Kopitz <mlkopitz@umich.edu>
2022-11-13 20:30:44 -0500

    refactor(package-info): Removed for now

--- lua/config/package-info.lua
+++ lua/config/package-info.lua
@@ -1,17 +1,31 @@
-require('package-info').setup
-{
-  colors = {
-    up_to_date = "#3C4048", -- Text color for up to date package virtual text
-    outdated = "#fc514e", -- Text color for outdated package virtual text
-  },
-  autostart = true, -- Whether to autostart when `package.json` is opened
-  hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
-  hide_unstable_versions = true, -- It hides unstable versions from version list e.g next-11.1.3-canary3
-
-  -- Can be `npm` or `yarn`. Used for `delete`, `install` etc...
-  -- The plugin will try to auto-detect the package manager based on
-  -- `yarn.lock` or `package-lock.json`. If none are found it will use the
-  -- provided one,                              if nothing is provided it will use `yarn`
-  package_manager = 'pnpm'
+require('package-info').setup {
+    autostart = true, -- Whether to autostart when `package.json` is opened
+    hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
+    hide_unstable_versions = false, -- It hides unstable versions from version list e.g next-11.1.3-canary3
+    -- Can be `npm`, `yarn`, or `pnpm`. Used for `delete`, `install` etc...
+    -- The plugin will try to auto-detect the package manager based on
+    -- `yarn.lock` or `package-lock.json`. If none are found it will use the
+    -- provided one, if nothing is provided it will use `yarn`
+    package_manager = 'pnpm'
 }
 
+-- Show dependency versions
+vim.keymap.set({ "n" }, "<LEADER>ns", require("package-info").show, { silent = true, noremap = true })
+
+-- Hide dependency versions
+vim.keymap.set({ "n" }, "<LEADER>nc", require("package-info").hide, { silent = true, noremap = true })
+
+-- Toggle dependency versions
+vim.keymap.set({ "n" }, "<LEADER>nt", require("package-info").toggle, { silent = true, noremap = true })
+
+-- Update dependency on the line
+vim.keymap.set({ "n" }, "<LEADER>nu", require("package-info").update, { silent = true, noremap = true })
+
+-- Delete dependency on the line
+vim.keymap.set({ "n" }, "<LEADER>nd", require("package-info").delete, { silent = true, noremap = true })
+
+-- Install a new dependency
+vim.keymap.set({ "n" }, "<LEADER>ni", require("package-info").install, { silent = true, noremap = true })
+
+-- Install a different dependency version
+vim.keymap.set({ "n" }, "<LEADER>np", require("package-info").change_version, { silent = true, noremap = true })
