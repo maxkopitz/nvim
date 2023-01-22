1f8fd2f7ca9e8bcd9af6e84a2e7e1dfd5b769cdf
Max Kopitz <mlkopitz@umich.edu>
2023-01-22 17:44:18 -0500

    Added config for vim-fugitive

---
+++ lua/config/vim-fugitive.lua
@@ -0,0 +1,9 @@
+vim.keymap.set('n', '<leader>gs', '<cmd>Git<cr>', { desc = 'Git status' })
+vim.keymap.set('n', '<leader>gw', '<cmd>Gwrite<cr>', { desc = 'Git add' })
+vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = 'Git commit' })
+vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = 'Git diff' })
+vim.keymap.set('n', '<leader>gpl', '<cmd>Git pull<cr>', { desc = 'Git pull' })
+vim.keymap.set('n', '<leader>gpu', '<cmd>15 split|term git push<cr>', { desc = 'Git push' })
+
+-- convert git to Git in command line mode
+vim.fn['utils#Cabbrev']('git', 'Git')
