852d94cc5f6ceb7a439a3164e23d9a3fb55b4e98
Max Kopitz <mlkopitz@umich.edu>
2023-01-22 16:31:40 -0500

    Added nvim version util and check

--- init.lua
+++ init.lua
@@ -5,11 +5,23 @@
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
+local utils = require 'utils'
+
 require 'core.config'
 require 'core.settings'
 require 'core.autocmd'
 require 'core.keymappings'
 require 'core.plugins'
+
+--> Checks for correct nvim veriosn <--
+local expected_ver = '0.8.2'
+local nvim_ver = utils.get_nvim_version()
+
+if nvim_ver ~= expected_ver then
+  local msg = string.format('Unsupported nvim version: expect %s, but got %s instead!', expected_ver, nvim_ver)
+  vim.api.nvim_err_writeln(msg)
+  return
+end
 
 -- Enable Comment.nvim
 require('Comment').setup()
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -11,4 +11,11 @@ function M.may_create_dir(dir)
   end
 end
 
+function M.get_nvim_version()
+  local actual_ver = vim.version()
+
+  local nvim_ver_str = string.format('%d.%d.%d', actual_ver.major, actual_ver.minor, actual_ver.patch)
+  return nvim_ver_str
+end
+
 return M
