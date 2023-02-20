b89ed1bfd4d4d3800edf2141642b70e316d0c83b
Max Kopitz <mlkopitz@umich.edu>
2023-02-19 20:42:50 -0500

    Removed print statement

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
-print('hi')
 
 local utils = require 'utils'
 
