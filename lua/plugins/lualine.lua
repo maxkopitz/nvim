1bf29b60e79db5557de951dc58ee9afe0898a67c
Max Kopitz <maxkopitz02@gmail.com>
2023-05-03 16:36:39 -0400

    Fixed opts lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,21 @@
 return {
   'nvim-lualine/lualine.nvim',
-  opts = {
-    icons_enabled = true,
-    component_separators = '|',
-    section_separators = '',
-    disabled_filetypes = { 'alpha', 'dashboard', 'NvimTree', 'Outline', 'netrw' },
-  },
+  config = function()
+    require('lualine').setup {
+      options = {
+        icons_enabled = false,
+        component_separators = '|',
+        section_separators = '',
+        disabled_filetypes = { 'alpha', 'dashboard', 'NvimTree', 'Outline', 'netrw' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+    }
+  end
 }
