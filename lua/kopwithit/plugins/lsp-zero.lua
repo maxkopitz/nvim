6296b6bd62188acd72a54ab6f9154c0146906c78
Max Kopitz <maxkopitz02@gmail.com>
2023-12-22 11:33:46 -0500

    Fixed copilot setup

--- lua/kopwithit/plugins/lsp-zero.lua
+++ lua/kopwithit/plugins/lsp-zero.lua
@@ -41,15 +41,20 @@ return {
         local cmp_format = lsp_zero.cmp_format()
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
         cmp.setup({
-            -- documentation says this is important.
-            -- I don't know why.
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = false,
+            sources = {
+                { name = 'copilot' },
+                { name = 'nvim_lsp' },
+            },
             formatting = cmp_format,
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-y>'] = cmp.mapping.confirm({
+                    -- documentation says this is important.
+                    -- I don't know why.
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = false,
+                }),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ['<Tab>'] = nil,
                 ['<S-Tab>'] = nil,
@@ -58,7 +63,6 @@ return {
 
         lsp_zero.on_attach(function(client, bufnr)
             local opts = { buffer = bufnr, remap = false }
-
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
