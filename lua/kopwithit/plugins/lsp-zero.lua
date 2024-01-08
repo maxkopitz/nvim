b9e172f64dc48b442ac8f6f4d23ddbc3b14b0648
Max Kopitz <maxkopitz02@gmail.com>
2024-01-08 17:14:33 -0500

    lsp zero

--- lua/kopwithit/plugins/lsp-zero.lua
+++ lua/kopwithit/plugins/lsp-zero.lua
@@ -1,66 +1,75 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  dependencies = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-  },
-  config = function()
-    local lsp = require('lsp-zero').preset('recommended')
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' },             -- Required
+        { 'williamboman/mason.nvim' },           -- Optional
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },                  -- Required
+        { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+        { 'L3MON4D3/LuaSnip' },                  -- Required
+    },
+    config = function()
+        local lsp = require('lsp-zero').preset('recommended')
 
-    lsp.ensure_installed({
-      'tsserver',
-      'eslint',
-      'lua_ls',
-    })
+        lsp.ensure_installed({
+            'tsserver',
+            'eslint',
+            'lua_ls',
+        })
 
-    require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-    local cmp = require('cmp')
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-    local cmp_mappings = lsp.defaults.cmp_mappings({
-      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-      ["<C-Space>"] = cmp.mapping.complete(),
-    })
+        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+        local cmp = require('cmp')
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local cmp_mappings = lsp.defaults.cmp_mappings({
+            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+            ["<C-Space>"] = cmp.mapping.complete(),
+        })
 
-    cmp_mappings['<Tab>'] = nil
-    cmp_mappings['<S-Tab>'] = nil
+        cmp_mappings['<Tab>'] = nil
+        cmp_mappings['<S-Tab>'] = nil
 
-    lsp.setup_nvim_cmp({
-      mapping = cmp_mappings
-    })
+        lsp.setup_nvim_cmp({
+            mapping = cmp_mappings
+        })
 
-    lsp.set_preferences({
-      suggest_lsp_servers = false,
-    })
+        lsp.set_preferences({
+            suggest_lsp_servers = false,
+        })
 
-    lsp.on_attach(function(client, bufnr)
-      local opts = {buffer = bufnr, remap = false}
+        local function on_list(options)
+            print(options)
+            print('test')
+            vim.fn.setqflist({}, ' ', options)
+            vim.api.nvim_command('cfirst')
+        end
+        lsp.on_attach(function(client, bufnr)
+            local opts = { buffer = bufnr, remap = false }
 
-      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-      vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-      vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    end)
+            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set("n", "gm", function()
+                vim.lsp.buf.implementation({ on_list = on_list })
+            end, opts)
+            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        end)
 
 
-    lsp.setup()
+        lsp.setup()
 
-    vim.diagnostic.config({
-      virtual_text = true
-    })
-  end,
+        vim.diagnostic.config({
+            virtual_text = true
+        })
+    end,
 }
--- lua/kopwithit/settings.lua
+++ lua/kopwithit/settings.lua
@@ -28,3 +28,5 @@ vim.g.mapleader = " "
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+vim.opt.colorcolumn = "80"
