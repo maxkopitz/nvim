a7b11b57cc12dbb218314490090739dcccf894fb
Maxwell Kopitz <mlkopitz@umich.edu>
2024-04-03 11:22:18 -0400

    wip: treesitter

--- lua/kopwithit/plugins/nvim-treesitter.lua
+++ lua/kopwithit/plugins/nvim-treesitter.lua
@@ -1,28 +1,86 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = {
-          "javascript",
-          "typescript",
-          "c",
-          "cpp",
-          "lua",
-          "rust",
-          "markdown",
-          "markdown_inline",
-        },
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = { "markdown" },
-        }
-      }
-    end,
-  },
-  {
-    "nvim-treesitter/playground"
-  },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects'
+
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = {
+                    "javascript",
+                    "typescript",
+                    "c",
+                    "cpp",
+                    "lua",
+                    "rust",
+                    "markdown",
+                    "markdown_inline",
+                },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = { "markdown" },
+                },
+                textobjects = {
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+
+                        goto_next_start = {
+                            ["]p"] = "@parameter.inner",
+                            ["]m"] = "@function.outer",
+                            ["]]"] = "@class.outer",
+                        },
+                        goto_next_end = {
+                            ["]M"] = "@function.outer",
+                            ["]["] = "@class.outer",
+                        },
+                        goto_previous_start = {
+                            ["[p"] = "@parameter.inner",
+                            ["[m"] = "@function.outer",
+                            ["[["] = "@class.outer",
+                        },
+                        goto_previous_end = {
+                            ["[M"] = "@function.outer",
+                            ["[]"] = "@class.outer",
+                        },
+                    },
+
+                    select = {
+                        enable = true,
+                        lookahead = true,
+
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+
+                            ["ac"] = "@conditional.outer",
+                            ["ic"] = "@conditional.inner",
+
+                            ["aa"] = "@parameter.outer",
+                            ["ia"] = "@parameter.inner",
+
+                            ["av"] = "@variable.outer",
+                            ["iv"] = "@variable.inner",
+                        },
+                    },
+
+                    swap = {
+                        enable = true,
+                        swap_next = {
+                            ["<leader>a"] = "@paramter.inner",
+                        },
+                        swap_previous = {
+                            ["<leader>A"] = "@parameter.inner",
+                        },
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "nvim-treesitter/playground"
+    },
 }
