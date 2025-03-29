47a82f105d940f69bb685bcd7986031652f2c51d
Maxwell Kopitz <accounts@maxwell.dev>
2025-03-29 13:12:58 -0500

    feat: telescope fzf loading

--- lua/kopwithit/plugins/telescope.lua
+++ lua/kopwithit/plugins/telescope.lua
@@ -6,6 +6,17 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
+        require('telescope').setup {
+            pickers = {
+                find_files = {
+                    theme = "ivy"
+                }
+            },
+            extension = {
+                fzf = {}
+            }
+        }
+        require('telescope').load_extension('fzf')
         local builtin = require('telescope.builtin')
 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
@@ -13,11 +24,11 @@ return {
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>en', function()
             require('telescope.builtin').find_files {
                 cwd = vim.fn.stdpath("config")
             }
         end)
-    end,
+    end
 }
