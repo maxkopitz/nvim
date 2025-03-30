fb40a87c065f5b58675bfa3d41424115eeb7aab1
Maxwell Kopitz <accounts@maxwell.dev>
2025-03-30 12:00:34 -0500

    feat: Mutli-grep

--- lua/kopwithit/plugins/harpoon.lua
+++ lua/kopwithit/plugins/harpoon.lua
@@ -37,6 +37,5 @@ return {
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
         vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
     end,
 }
--- lua/kopwithit/plugins/telescope.lua
+++ lua/kopwithit/plugins/telescope.lua
@@ -17,14 +17,14 @@ return {
             }
         }
         require('telescope').load_extension('fzf')
+
+        local mutli_ripgrep = require "kopwithit.telescope.multi-ripgrep"
         local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>fd', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', mutli_ripgrep)
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>en', function()
             require('telescope.builtin').find_files {
                 cwd = vim.fn.stdpath("config")
@@ -35,5 +35,6 @@ return {
                 cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
             }
         end)
+
     end
 }
